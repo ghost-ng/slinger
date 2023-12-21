e3ec61912f558f07a7dffb69365404cdee4026f7
ghost-ng <ghost-ng@outlook.com>
2023-12-21 12:18:04 -0500

    now properly hangling plugin args

--- slinger/plugins/my_plugin.py
+++ slinger/plugins/my_plugin.py
@@ -1,18 +1,18 @@
 # my_plugin.py
 from slinger.lib.plugin_base import PluginBase
 import argparse
-from slinger.utils.cli import app_cmds_parser
 
 class MyPlugin(PluginBase):
     def get_parser(self):
         # define a new subparser to return to merge with the main parser
-        subparsers = app_cmds_parser.add_subparsers(dest='command')
-        plugincmd_parser = subparsers.add_parser("plugincmd", help="My subparser")
-        plugincmd_parser.add_argument("--plugincmd", help="My argument")
-        
+        parser = argparse.ArgumentParser(add_help=False)
+        subparsers = parser.add_subparsers(dest='command')
+        plugincmd_parser = subparsers.add_parser("plugincmd", help="My plugin subparser")
+        plugincmd_parser.add_argument("--plugincmd", help="My plugin argument")
+        return parser
 
     def execute(self, args):
-        print("Executing MyPlugin with args:", args)
+        print(f"Executing {args.command} with {args.plugincmd}")
 
     def get_commands(self):
-        return ["mycmd"]
+        return ["plugincmd"]
