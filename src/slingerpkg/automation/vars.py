abfca5565d4f82a4d1a3c1710758b9c47e458835
ghost-ng <ghost-ng@outlook.com>
2025-06-19 11:29:36 -0500

    ai agent init attempt

---
+++ src/slingerpkg/automation/vars.py
@@ -0,0 +1,10 @@
+import re
+from unittest.mock import MagicMock
+
+from slingerpkg.utils.cli import extract_commands_and_args, setup_cli_parser
+ANSI   = r'\x1b\[[0-9;]*[A-Za-z]'
+PROMPT = re.compile(rf'(?:{ANSI})*\[sl\].*?> ')
+
+mock_client = MagicMock()
+parser = setup_cli_parser(mock_client)
+COMMAND_LIST = ", ".join(extract_commands_and_args(parser).keys())
